82625a770b5e61f29dc4c37b2f4d6d1bf3fc27a0
Marco.Polita <polita.marco@gmail.com>
2016-03-31 19:45:49 +0200

    first working espresso test

--- app/src/androidTest/java/efestoarts/gameoflife/ApplicationTest.java
+++ app/src/androidTest/java/efestoarts/gameoflife/ApplicationTest.java
@@ -1,13 +1,43 @@
 package efestoarts.gameoflife;
 
 import android.app.Application;
+import android.support.test.espresso.ViewAssertion;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
+import android.test.suitebuilder.annotation.LargeTest;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import efestoarts.gameoflife.presenter.MainActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ApplicationTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void view_setText() throws Throwable {
+        onView(withId(R.id.test_text)).check(matches(withText("Game of life")));
+        mActivityRule.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mActivityRule.getActivity().setText("this is text");
+            }
+        });
+
+        onView(withId(R.id.test_text)).check(matches(withText("this is text")));
+
     }
 }
